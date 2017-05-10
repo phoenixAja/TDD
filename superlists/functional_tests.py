44c45094e2a5fc4ccd53f4021d7771d54a0cb189
phoenixAja <phoenix@phoenixlogan.net>
2017-05-10 16:49:25 -0700

    final push of the day

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -36,6 +36,8 @@ class NewVisitorTest(unittest.TestCase):
 	# 1) buy peacock feathers as an item on the to-do list
         inputbox.send_keys(Keys.ENTER)
 
+        import time
+        time.sleep(10) 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
