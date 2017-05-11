d1fc8703914c31811663a7d1df414dd72758d9c6
phoenixAja <phoenix@phoenixlogan.net>
2017-05-11 16:17:33 -0700

    editing more functional tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -36,25 +36,30 @@ class NewVisitorTest(unittest.TestCase):
 	# 1) buy peacock feathers as an item on the to-do list
         inputbox.send_keys(Keys.ENTER)
 
-        import time
-        time.sleep(10) 
+        # import time
+        # time.sleep(10) 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                   any(row.text == '1: Buy peacock feathers' for row in rows),
-                   "New to-do item did not appear in the table"
-        )
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+             
 
 	# There is still a text box inviting her to add another item. 
 	# She enters "Use peacock feathers to make a fly"
-        self.fail('Finish the test')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
 	# The page updates againm and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+ 
 
 	# Edith wonders whether the site will remember her list. Then 
 	# She sees that the site has created a unique URL for her --there
 	# is some explanatory text to that effect
-
+        self.fail('finish the test!')
 	# She visits that URL - her to-do list is there. 
 
 	# She is satisfied
