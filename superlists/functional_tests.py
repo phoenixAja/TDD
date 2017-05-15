9b2b936be5f652a5b85173043d8ef680eae17ddc
phoenixAja <phoenix@phoenixlogan.net>
2017-05-15 16:51:32 -0700

    made updates on testing script

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -4,66 +4,60 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
     
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
-    def tearDown(self):
-        self.browser.quit()
+	def tearDown(self):
+		self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
 	
-	# Edith had heard about a cool new online to-dp app. She goes
-	# to check out it's homepage
-        self.browser.get('http://localhost:8000')
+	    # Edith had heard about a cool new online to-dp app. She goes
+	    # to check out it's homepage
+		self.browser.get('http://localhost:8000')
 
-	# She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+	    # She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
-	# She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-                   inputbox.get_attribute('placeholder'),
-                   'Enter a To-Do item'
-        ) 
+	    # She is invited to enter a to-do item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'),
+						'Enter a To-Do item'
+						) 
 
-	# She types "buy peacock feathers" into a text box 
-        inputbox.send_keys('Buy peacock feathers')
-  
-	# When she hits enter, the page updates, and now the page lists
-	# 1) buy peacock feathers as an item on the to-do list
-        inputbox.send_keys(Keys.ENTER)
-
-        # import time
-        # time.sleep(10) 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+	    # She types "buy peacock feathers" into a text box 
+	    # When she hits enter, the page updates, and now the page lists
+	    # 1) buy peacock feathers as an item on the to-do list
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
              
 
-	# There is still a text box inviting her to add another item. 
-	# She enters "Use peacock feathers to make a fly"
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-
-	# The page updates againm and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+	    # There is still a text box inviting her to add another item. 
+	    # She enters "Use peacock feathers to make a fly
+	    # The page updates againm and now shows both items on her list
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
  
 
-	# Edith wonders whether the site will remember her list. Then 
-	# She sees that the site has created a unique URL for her --there
-	# is some explanatory text to that effect
-        self.fail('finish the test!')
-	# She visits that URL - her to-do list is there. 
+	    # Edith wonders whether the site will remember her list. Then 
+	    # She sees that the site has created a unique URL for her --there
+	    # is some explanatory text to that effect
+		self.fail('finish the test!')
+	    # She visits that URL - her to-do list is there. 
 
-	# She is satisfied
+	    # She is satisfied
 
 
 if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+	unittest.main(warnings='ignore')
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,29 +3,82 @@ from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.conf import settings
+if not settings.configured:
+	settings.configure()
+from lists.models import Item
+
 
 class HomePageTest(TestCase):
-    
-    def test_root_url_resolves_to_home_page(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest() #http request object, what django sees when a users browser asks for a page
+	# test to make sure '/'' resolves to the home page
+	def test_root_url_resolves_to_home_page(self):
 
-        response = home_page(request) #we pass above to home_page view, which gives us a response, then 
-				      #we assert that the .content of the response - which is the HTML we are
-                                      #sending to the user has certain properties
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
 
-        expected_html = render_to_string('home.html')
+	def test_home_page_returns_correct_html(self):
+		#http request object, what django sees when a users browser asks for a page
 
-        self.assertEqual(response.content.decode(), expected_html) # decode converts response.content bytes into 
-                                                                   # python unicode string, (comparing strings w/
-                                                                   # strings instead of comparing bytes
+		request = HttpRequest() 
 
-        #self.assertTrue(response.content.startswith(b'<html>')) #we want it to start with an <html> tag which 
-                                                                #gets closed at the end. notice raw bytes
+		#we pass above to home_page view, which gives us a response, then 
+        #we assert that the .content of the response - which is the HTML we are
+        #sending to the user has certain properties
+		response = home_page(request) 
+		expected_html = render_to_string('home.html')
 
-        #self.assertIn(b'<title>To-Do Lists</title>', response.content) #we want a title tag somewhere in the middle
-								       #with the words to-do list
+        # decode converts response.content bytes into 
+        # python unicode string, (comparing strings w/
+        # strings instead of comparing bytes
+		self.assertEqual(response.content.decode(), expected_html) 
+                                                                   
+        #we want it to start with an <html> tag which 
+        #gets closed at the end. notice raw bytes
+        #self.assertTrue(response.content.startswith(b'<html>')) 
+                                                                
+        #we want a title tag somewhere in the middle
+        #with the words to-do list
+        #self.assertIn(b'<title>To-Do Lists</title>', response.content)      
         #self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def test_home_page_only_saves_items_when_necessary(self):
+		request = HttpRequest()
+		home_page(request)
+		self.assertEqual(Item.objects.count(),0)
+
+
+class ItemModelTest(TestCase):
+
+	# testing data storage and retrieval
+	def test_saving_and_retrieving_items(self):
+
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
